edea11504c41f39f9ccf9b6862b6ec8f38c7d9a6
danny galimi <dannygalimi13@aim.com>
2022-06-02 10:28:14 -0400

    commit tree 01eae2803064fe5e04810d60682ca3912cd00d90

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
 
+def sort_array_asc(array)
+    array.sort do |a,b|
+        a <=> b
+    end
+end 
+
+def sort_array_desc(array)
+    array.sort do |a,b|
+        b <=> a
+    end
+end 
+
+def sort_array_char_count(array)
+    array.sort do |a,b|
+        a.length <=> b.length
+    end
+end 
+
+def swap_elements(array)
+    x = array[1]
+    array[1] = array[2]
+    array[2] = x
+    array
+end 
+
+def reverse_array(array)
+    array.reverse
+end 
+
+def kesha_maker(array)
+    new_array = []
+    array.each do |x|
+        x[2] = "$"
+        new_array << x
+    end 
+    new_array
+end 
+
+def find_a(array)
+    array.select {|x| x.start_with?("a")}
+end 
+
+def sum_array(array)
+    array.sum
+end 
+
+def add_s(array)
+    array.map do |x|
+        if array[1] == x
+            x
+        else
+            x + "s"
+        end
+    end 
+end 
